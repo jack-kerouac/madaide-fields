927789a04f06011c705e2b22906d135514715c61
Florian Rampp <Florian.Rampp@gmail.com>
2014-03-06 12:32:36 +0100

    added output of yeoman's angular generator

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('madaideFieldsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
